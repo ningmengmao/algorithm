fb36406b48d88c48fc17eb809e8c224e90ea559d
ningmengmao <ge672828794@gmail.com>
2021-05-30 22:51:38 +0800

    用队列实现栈

--- src/main/java/com/ningmeng/leetcode/Number1074.java
+++ src/main/java/com/ningmeng/leetcode/Number1074.java
@@ -9,34 +9,51 @@ import java.util.Map;
  * @Version 1.0
  */
 public class Number1074 {
-		public int numSubmatrixSumTarget(int[][] matrix, int target) {
-			int ans = 0;
-			int m = matrix.length, n = matrix[0].length;
-			for (int i = 0; i < m; ++i) { // 枚举上边界
-				int[] sum = new int[n];
-				for (int j = i; j < m; ++j) { // 枚举下边界
-					for (int c = 0; c < n; ++c) {
-						sum[c] += matrix[j][c]; // 更新每列的元素和
-					}
-					ans += subarraySum(sum, target);
-				}
-			}
-			return ans;
-		}
 
-		public int subarraySum(int[] nums, int k) {
-			Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-			map.put(0, 1);
-			int count = 0, pre = 0;
-			for (int x : nums) {
-				pre += x;
-				if (map.containsKey(pre - k)) {
-					count += map.get(pre - k);
+	public static void main(String[] args) {
+		var arr = new int[][] {
+				{0,1,0},
+				{1,2,2},
+				{0,1,0}
+		};
+		var main = new Number1074();
+		System.out.println(main.numSubmatrixSumTarget(arr, 0));
+	}
+
+	public int numSubmatrixSumTarget(int[][] matrix, int target) {
+		int ans = 0;
+		// m是x, n是y
+		int m = matrix.length, n = matrix[0].length;
+
+		// 从第一行开始
+		// 0,0 -> 0,1 -> 0,2
+		// 1,1 -> 1,2
+		// 2,2
+		for (int i = 0; i < m; ++i) { // 枚举上边界
+			int[] sum = new int[n];
+			for (int j = i; j < m; ++j) { // 枚举下边界
+				for (int c = 0; c < n; ++c) {
+					sum[c] += matrix[j][c]; // 更新每列的元素和
 				}
-				map.put(pre, map.getOrDefault(pre, 0) + 1);
+				ans += subarraySum(sum, target);
 			}
-			return count;
 		}
+		return ans;
+	}
+
+	public int subarraySum(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(0, 1);
+		int count = 0, pre = 0;
+		for (int x : nums) {
+			pre += x;
+			if (map.containsKey(pre - k)) {
+				count += map.get(pre - k);
+			}
+			map.put(pre, map.getOrDefault(pre, 0) + 1);
+		}
+		return count;
+	}
 
 
 }
---
+++ src/main/java/com/ningmeng/leetcode/Number225.java
@@ -0,0 +1,50 @@
+package com.ningmeng.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author ningmengmao
+ * @Date 2021-05-30 22:50:48
+ * @Version 1.0
+ */
+public class Number225 {
+
+	class MyStack {
+		Queue<Integer> stack;
+		Queue<Integer> temp;
+
+
+		/** Initialize your data structure here. */
+		public MyStack() {
+			stack = new LinkedList<>();
+			temp = new LinkedList<>();
+		}
+
+		/** Push element x onto stack. */
+		public void push(int x) {
+			while(!stack.isEmpty()) {
+				temp.offer(stack.poll());
+			}
+			stack.offer(x);
+			while(!temp.isEmpty()) {
+				stack.offer(temp.poll());
+			}
+		}
+
+		/** Removes the element on top of the stack and returns that element. */
+		public int pop() {
+			return stack.poll();
+		}
+
+		/** Get the top element. */
+		public int top() {
+			return stack.element();
+		}
+
+		/** Returns whether the stack is empty. */
+		public boolean empty() {
+			return stack.isEmpty();
+		}
+	}
+}
